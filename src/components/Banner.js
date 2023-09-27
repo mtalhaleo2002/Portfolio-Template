eed279a70e46e4eb5ddf71f3a170e05ce8ed7feb
Mohammad Talha <m.talhaleo2002@gmail.com>
2023-09-27 21:36:00 +0500

    Skills File In Process

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -52,7 +52,7 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome To My Portfolio</span>
-                        <h1>{`Hi I,m Coding With Chaudhary`}<span className="wrap">{text}</span></h1>
+                        <h1>{`Hi I,m Mohammad Talha `}<span className="wrap">{text}</span></h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                         <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
                     </Col>
